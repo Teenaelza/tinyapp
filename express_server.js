22105692441fdcd30ceb3e658ec0f6985577d41e
Teenaelza <teena.thaliath@gmail.com>
2021-08-30 22:16:42 +0000

    set up server using express

---
+++ express_server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = "3000";
+const app = express();
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+app.listen(PORT, () => console.log(`This server is listening to ${PORT}!`));
