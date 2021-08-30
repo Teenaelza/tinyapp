227e6d8cecb308df221237fd66009726138aaccf
Teenaelza <teena.thaliath@gmail.com>
2021-08-30 23:31:25 +0000

    checked whether a variable created is accessible in another

--- express_server.js
+++ express_server.js
@@ -14,4 +14,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+// app.get("/set", (req, res) => {
+//   let a = 0;
+//   res.send(`a=${a}`);
+// });
+// app.get("/fetch", (req, res) => {
+//   res.send(`a=${a}`);
+// });
 app.listen(PORT, () => console.log(`This server is listening to ${PORT}!`));
