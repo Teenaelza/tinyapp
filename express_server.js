22fdb1d92975616051e1231585b3dabe3dbc8000
Teenaelza <teena.thaliath@gmail.com>
2021-08-31 00:45:01 +0000

    adding a second route and template to display a single URL and its shortened form

--- express_server.js
+++ express_server.js
@@ -9,18 +9,28 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+//welcome page
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+//Hello Page
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
+//url index page : directs to the url-index template page
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const short = req.params.shortURL;
+  const templateVars = { shortURL: short, longURL: urlDatabase[short] };
+  res.render("urls_show", templateVars);
+});
+//display the url object
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => console.log(`This server is listening to ${PORT}!`));
