a7f31321ba1b44f112a373605da7a13d0c458880
Teenaelza <teena.thaliath@gmail.com>
2021-08-31 23:19:10 +0000

    Implement functionality to edit a resource

--- express_server.js
+++ express_server.js
@@ -56,4 +56,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   console.log(`deleted the url ${short}`);
   res.redirect("/urls");
 });
+app.post("/urls/:shortURL/edit", (req, res) => {
+  const short = req.params.shortURL;
+  urlDatabase[short] = req.body.longURL;
+  console.log(`updated the url ${urlDatabase[short]}`);
+  res.redirect("/urls");
+});
 app.listen(PORT, () => console.log(`This server is listening to ${PORT}!`));
