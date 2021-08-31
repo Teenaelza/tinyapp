89d8d85d99f7e52d36e26f548bc942f3f80948b9
Teenaelza <teena.thaliath@gmail.com>
2021-08-31 21:45:42 +0000

    Implement delete url functionality

--- express_server.js
+++ express_server.js
@@ -10,32 +10,28 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-app.use(bodyParser.urlencoded({ extended: true }));
-const generateRandomString = () => {};
+const generateRandomString = () => {
+  return Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, "")
+    .substr(0, 6);
+};
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/u/:shortURL", (req, res) => {
   const short = req.params.shortURL;
   const longURL = urlDatabase[short];
   res.redirect(longURL);
-});
-
-//welcome page
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
-//Hello Page
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
 });
 //url index page : directs to the url-index template page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+//request to create a new short urlDatabase;
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 //it will display the long url corresponding to the short url parameter that is send via request
 app.get("/urls/:shortURL", (req, res) => {
   const short = req.params.shortURL;
@@ -47,7 +43,17 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.post("/urls", (req, res) => {
+  const newShort = generateRandomString();
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  console.log(newShort);
+  urlDatabase[newShort] = req.body.longURL;
+  console.log(`added a new url: ${urlDatabase[newShort]}`);
+  res.redirect("/urls");
+});
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const short = req.params.shortURL;
+  delete urlDatabase[short];
+  console.log(`deleted the url ${short}`);
+  res.redirect("/urls");
 });
 app.listen(PORT, () => console.log(`This server is listening to ${PORT}!`));
