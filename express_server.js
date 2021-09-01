f337681c0b800c7a5069c04733a0bfc65ba8b67d
Teenaelza <teena.thaliath@gmail.com>
2021-09-01 04:06:40 +0000

    Add the login Form and the Login Route

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@
  */
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const PORT = "3000";
 const app = express();
 app.set("view engine", "ejs");
@@ -17,7 +18,7 @@ const generateRandomString = () => {
     .substr(0, 6);
 };
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.get("/u/:shortURL", (req, res) => {
   const short = req.params.shortURL;
   const longURL = urlDatabase[short];
@@ -25,23 +26,35 @@ app.get("/u/:shortURL", (req, res) => {
 });
 //url index page : directs to the url-index template page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  console.log("cookie:", req.cookies);
+  const templateVars = {
+    urls: urlDatabase,
+  };
+  if (req.cookies) {
+    templateVars["username"] = req.cookies["username"];
+  }
   res.render("urls_index", templateVars);
 });
 //request to create a new short urlDatabase;
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 //it will display the long url corresponding to the short url parameter that is send via request
 app.get("/urls/:shortURL", (req, res) => {
   const short = req.params.shortURL;
-  const templateVars = { shortURL: short, longURL: urlDatabase[short] };
+  const templateVars = {
+    username: req.cookies["username"],
+    shortURL: short,
+    longURL: urlDatabase[short],
+  };
   res.render("urls_show", templateVars);
 });
 //display the url object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+//index page
 app.post("/urls", (req, res) => {
   const newShort = generateRandomString();
   console.log(req.body); // Log the POST request body to the console
@@ -50,16 +63,26 @@ app.post("/urls", (req, res) => {
   console.log(`added a new url: ${urlDatabase[newShort]}`);
   res.redirect("/urls");
 });
+//when delete button is clicked
 app.post("/urls/:shortURL/delete", (req, res) => {
   const short = req.params.shortURL;
   delete urlDatabase[short];
   console.log(`deleted the url ${short}`);
   res.redirect("/urls");
 });
+//edit link is clicked
 app.post("/urls/:shortURL/edit", (req, res) => {
   const short = req.params.shortURL;
   urlDatabase[short] = req.body.longURL;
   console.log(`updated the url ${urlDatabase[short]}`);
+  res.redirect("/urls");
+});
+//login button is clicked
+app.post("/login", (req, res) => {
+  //console.log(req.body.username);
+  const cookieValue = req.body.username;
+  res.cookie("username", cookieValue);
+  console.log(`set cookie to ${cookieValue}`);
   res.redirect("/urls");
 });
 app.listen(PORT, () => console.log(`This server is listening to ${PORT}!`));
