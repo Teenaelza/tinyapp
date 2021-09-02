c7a90059264eb3b256d5ba6d4c3a5144b1b7ce0d
Teenaelza <teena.thaliath@gmail.com>
2021-09-02 05:36:40 +0000

    Update the Logout Handler

--- express_server.js
+++ express_server.js
@@ -29,7 +29,6 @@ const generateRandomString = (length) => {
 };
 //function to check whether the email and password is not empty
 const emailValidation = (email, password) => {
-  console.log("email validate");
   if (email.trim() && password.trim()) {
     return true;
   }
@@ -37,12 +36,21 @@ const emailValidation = (email, password) => {
 };
 //function to check the user already exists
 const userExist = (email) => {
-  for (let user in users) {
-    if (user.email === email) {
+  for (key in users) {
+    if (users[key].email === email) {
       return true;
     }
-    return false;
   }
+  return false;
+};
+//function to check the user and password are equal
+const passwordValidation = (email, password) => {
+  for (user in users) {
+    if (users[user].email === email && users[user].password === password) {
+      return users[user].id;
+    }
+  }
+  return false;
 };
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -65,7 +73,11 @@ app.get("/urls", (req, res) => {
 });
 //request to create a new short urlDatabase;
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = {};
+  if (req.cookies) {
+    const userIdKey = req.cookies["user_id"];
+    templateVars["user"] = users[userIdKey];
+  }
   res.render("urls_new", templateVars);
 });
 app.get("/register", (req, res) => {
@@ -79,11 +91,15 @@ app.get("/login", (req, res) => {
 //it will display the long url corresponding to the short url parameter that is send via request
 app.get("/urls/:shortURL", (req, res) => {
   const short = req.params.shortURL;
+
   const templateVars = {
-    username: req.cookies["username"],
     shortURL: short,
     longURL: urlDatabase[short],
   };
+  if (req.cookies) {
+    const userIdKey = req.cookies["user_id"];
+    templateVars["user"] = users[userIdKey];
+  }
   res.render("urls_show", templateVars);
 });
 //display the url object
@@ -121,10 +137,25 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 });
 //login button is clicked
 app.post("/login", (req, res) => {
-  //console.log(req.body.username);
-  //const cookieValue = req.body.username;
-  //res.cookie("username", cookieValue);
-  //console.log(`set cookie to ${cookieValue}`);
+  console.log(req.body);
+  const password = req.body.password;
+  const email = req.body.email;
+  console.log(users);
+  if (!emailValidation(email, password)) {
+    return res.status(400).send("Email/Password is empty");
+  }
+  if (!userExist(email)) {
+    console.log(userExist(email));
+    return res.status(403).send("User with the emailid not Found");
+  }
+  const userid = passwordValidation(email, password);
+  if (!userid) {
+    return res.status(403).send("User and password doesnot match");
+  }
+  res.cookie("user_id", userid);
+  console.log(`set usedid cookie to ${userid}`);
+
+  console.log("new user login:", email);
   res.redirect("/urls");
 });
 
@@ -134,16 +165,11 @@ app.post("/register", (req, res) => {
   const password = req.body.password;
   const email = req.body.email;
   if (!emailValidation(email, password)) {
-    res.status(400);
-    res.render("error", { error: "Email/Password is empty" });
+    return res.status(400).send("Email/Password is empty");
   }
-  if (userExist) {
-    res.status(400);
-    res.render("error", {
-      error: "User Already Exists",
-    });
+  if (userExist(email)) {
+    return res.status(400).send("User already exists");
   }
-
   const id = generateRandomString(8);
   users[id] = { id, email, password };
   res.cookie("user_id", id);
